59b608bba730552098b9e043e9b2f132f67e45d1
dannymcq2 <mcq.danny@icloud.com>
2024-09-18 18:59:25 -0400

    update

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,18 +1,14 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = {
-  mode: 'development', 
-  entry: './src/js/index.js', 
+  entry: './src/js/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
-
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -23,7 +19,6 @@ module.exports = {
           },
         },
       },
-
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
@@ -31,39 +26,9 @@ module.exports = {
     ],
   },
   plugins: [
-   
     new HtmlWebpackPlugin({
       template: './src/index.html',
       title: 'PWA Text Editor',
     }),
-    
-
-    new WebpackPwaManifest({
-      name: 'PWA Text Editor',
-      short_name: 'JATE',
-      description: 'A Progressive Web Application Text Editor',
-      background_color: '#ffffff',
-      theme_color: '#31a9e1',
-      start_url: './',
-      display: 'standalone',
-      icons: [
-        {
-          src: path.resolve('src/images/logo.png'),
-          sizes: [96, 128, 192, 256, 384, 512], 
-          destination: path.join('icons'),
-        },
-      ],
-    }),
-    
-    new InjectManifest({
-      swSrc: './src-sw.js', 
-      swDest: 'service-worker.js', 
-    }),
   ],
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 3001, 
-    open: true, 
-  },
 };
