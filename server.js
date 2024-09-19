4ef5202055770bb914457b236dd25d940b977fb9
dannymcq2 <mcq.danny@icloud.com>
2024-09-18 20:19:03 -0400

    server.js

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const path = require('path'); 
-
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 
+app.use(express.static('../client/dist'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
