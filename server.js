f06bdcb0fc06bcb8c74553c1e7eebe34196680ac
dannymcq2 <mcq.danny@icloud.com>
2024-09-18 20:28:40 -0400

    js

--- server.js
+++ server.js
@@ -1,30 +1,18 @@
-const path = require('path');
 const express = require('express');
+const path = require('path'); 
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static('../client/dist'));
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
-});
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
 });
 
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
-});
-require('./server/routes/htmlRoutes')(app);
-
-app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
